bf274a3631c440609a8871fb5a66fedf3c842800
BigPete93 <tjpetersen.of.florida@gmail.com>
2020-02-06 14:07:38 -0500

    Created a timed repeater in Main

--- src/main/java/org/mThree/Main.java
+++ src/main/java/org/mThree/Main.java
@@ -1,5 +1,9 @@
 package org.mThree;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Hello world!
  *
@@ -7,7 +11,21 @@ package org.mThree;
 public class Main
 {
     public static void main( String[] args ) throws Exception {
-        JsonParser.JsonParse();
+
         System.out.println( "Hello World!" );
+
+        // Create a service with 3 threads.
+        ScheduledExecutorService execService = Executors.newScheduledThreadPool(3);
+
+        // Schedule a task to run every 5 minutes with no initial delay.
+        execService.scheduleAtFixedRate(new Runnable() {
+            public void run() {
+                try {
+                    JsonParser.JsonParse();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }, 0L, 5L, TimeUnit.MINUTES);
     }
 }
