70f63e0568508732694b7cd241c5d839b6d99b0a
jean-pierre <jpsalazar_23@hotmail.com>
2020-02-07 10:48:53 -0500

    Included Rest of API Set Methods

--- src/main/java/org/mThree/Controller.java
+++ src/main/java/org/mThree/Controller.java
@@ -2,41 +2,74 @@ package org.mThree;
 
 import java.sql.*;
 
-/**
-     *
-     * @author pooya
-     */
     public class Controller {
 
-        static private Connection con;
-        { try { con = DriverManager.getConnection(
-                "jdbc:mysql://3.89.243.51:3306/employees","mysql","mysql");
-            } catch (SQLException e) { e.printStackTrace();} }
+        void setDataByWeek(String s, double open, double high, double low, double close, int volume){
+            try(Connection con = DriverManager.getConnection(
+                    "jdbc:mysql://3.89.243.51:3306/employees","mysql","mysql");
+                    PreparedStatement insertByWeek =
+                            con.prepareStatement("INSERT INTO /*table_name*/ VALUES (?, ?, ?, ?, ?, ?) ")
+            ){
+                insertByWeek.setString(1, s);
+                insertByWeek.setDouble(2, open);
+                insertByWeek.setDouble(3, high);
+                insertByWeek.setDouble(4, low);
+                insertByWeek.setDouble(5, close);
+                insertByWeek.setInt(6, volume);
 
+                insertByWeek.executeUpdate();
+            } catch (SQLException e) {e.printStackTrace();}
+        }
 
-    public static void main(String[] args) {
+        void setDataByDay(String s, double open, double high, double low, double close, int volume){
+            try(Connection con = DriverManager.getConnection(
+                    "jdbc:mysql://3.89.243.51:3306/employees","mysql","mysql");
+                PreparedStatement insertByDay =
+                        con.prepareStatement("INSERT INTO /*table_name*/ VALUES (?, ?, ?, ?, ?, ?) ")
+            ){
+                insertByDay.setString(1, s);
+                insertByDay.setDouble(2, open);
+                insertByDay.setDouble(3, high);
+                insertByDay.setDouble(4, low);
+                insertByDay.setDouble(5, close);
+                insertByDay.setInt(6, volume);
+
+                insertByDay.executeUpdate();
+            } catch (SQLException e) {e.printStackTrace();}
+        }
+
+        void setDataBy5Min(String s, double open, double high, double low, double close, int volume){
+            try(Connection con = DriverManager.getConnection(
+                    "jdbc:mysql://3.89.243.51:3306/employees","mysql","mysql");
+                PreparedStatement insertBy5Min =
+                        con.prepareStatement("INSERT INTO /*table_name*/ VALUES (?, ?, ?, ?, ?, ?) ")
+            ){
+                insertBy5Min.setString(1, s);
+                insertBy5Min.setDouble(2, open);
+                insertBy5Min.setDouble(3, high);
+                insertBy5Min.setDouble(4, low);
+                insertBy5Min.setDouble(5, close);
+                insertBy5Min.setInt(6, volume);
+
+                insertBy5Min.executeUpdate();
+            } catch (SQLException e) {e.printStackTrace();}
+        }
+
+        public static void main(String[] args) {
             System.out.println("asdas");
             try{
+                Connection con = DriverManager.getConnection(
+                        "jdbc:mysql://3.89.243.51:3306/employees","mysql","mysql");
+
                 Class.forName("com.mysql.jdbc.Driver");
                 Statement stmt=con.createStatement();
                 ResultSet rs=stmt.executeQuery("SELECT * FROM employees");
+
                 while(rs.next())
                     System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));
                 con.close();
             }catch(Exception e){ System.out.println(e);}
         }
-
-        void setDataByWeek(String s, double open, double high, double low, double close, int volume) throws SQLException {
-            PreparedStatement stmt = con.prepareStatement("INSERT INTO table_name VALUES (?, ?, ?, ?, ?, ?) ");
-
-            stmt.setString(1,s);
-            stmt.setDouble(2, open);
-            stmt.setDouble(3, high);
-            stmt.setDouble(4, low);
-            stmt.setDouble(5, close);
-            stmt.setInt(6, volume);
-        }
-
 
     }
 
