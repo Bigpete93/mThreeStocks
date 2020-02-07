de7bbe4a109e57cea175c1a83107c8d68decc5a6
Jared Hilliard <39249055+jaredhilliard@users.noreply.github.com>
2020-02-07 14:54:40 -0500

    Added the line to grant user permissions

--- classProjectSchema.sql
+++ classProjectSchema.sql
@@ -7,3 +7,7 @@ CREATE TABLE week_data (week_date DATE, open FLOAT, high FLOAT, low FLOAT, close
 CREATE TABLE day_data (day_date DATE, open FLOAT, high FLOAT, low FLOAT, close FLOAT, volume BIGINT);
 
 CREATE TABLE 5_min_data (day_date DATE, day_time TIME, open FLOAT, high FLOAT, low FLOAT, close FLOAT, volume BIGINT);
+
+--The ip here will need to change.
+GRANT ALL ON project TO 'root'@'3.83.240.60';
+GRANT ALL ON project TO 'root'@'localhost';
