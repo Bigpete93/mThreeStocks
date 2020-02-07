6d3a8b4acc92794a2ac1f5588b1d7ae915201ece
Jared Hilliard <39249055+jaredhilliard@users.noreply.github.com>
2020-02-07 15:16:51 -0500

    Changed 5_min_data to five_min_data

--- classProjectSchema.sql
+++ classProjectSchema.sql
@@ -6,7 +6,7 @@ CREATE TABLE week_data (week_date DATE, open FLOAT, high FLOAT, low FLOAT, close
 
 CREATE TABLE day_data (day_date DATE, open FLOAT, high FLOAT, low FLOAT, close FLOAT, volume BIGINT, PRIMARY KEY (day_date));
 
-CREATE TABLE 5_min_data (5_min_date DATE, 5_min_time TIME, open FLOAT, high FLOAT, low FLOAT, close FLOAT, volume BIGINT, PRIMARY KEY (5_min_date, 5_min_time));
+CREATE TABLE five_min_data (five_min_date DATE, five_min_time TIME, open FLOAT, high FLOAT, low FLOAT, close FLOAT, volume BIGINT, PRIMARY KEY (five_min_date, five_min_time));
 
 --The ip here will need to change.
 GRANT ALL ON project TO 'root'@'3.83.240.60';
