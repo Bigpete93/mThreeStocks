1da9a55a3a849eef2bc819d288e048d85196aa9b
AzuResolute <36246550+AzuResolute@users.noreply.github.com>
2020-02-11 19:08:47 -0500

    Finalized Day Trade Platform

--- src/main/java/org/mThree/Client/GetData.js
+++ src/main/java/org/mThree/Client/GetData.js
@@ -24,11 +24,8 @@ let state = {
       let day = dformat.getDay();
       if (day === 5 || d === state.data.Weekly[0].date)
         return d;
-      else if (day === 6) 
-        dformat.setDate(dformat.getDate() - 1);
-      else 
-        dformat.setDate(dformat.getDate() - (2 + day));
-      
+      dformat.setDate(dformat.getDate() - (day === 6 ? 1 : 2 + day));
+
       return `${new Date(dformat.getTime() - (dformat.getTimezoneOffset() * 60000 ))
                 .toISOString()
                 .split("T")[0]}`;
@@ -37,10 +34,10 @@ let state = {
       let dformat = new Date(`${d} GMT-05:00`);
       let day = dformat.getDay();
       if(day === 0)
-      dformat.setDate(dformat.getDate() - 2);
-      else if (day === 6) 
-      dformat.setDate(dformat.getDate() - 1);
-      
+        dformat.setDate(dformat.getDate() - 2);
+      else if (day === 6)
+        dformat.setDate(dformat.getDate() - 1);
+
       return `${new Date(dformat.getTime() - (dformat.getTimezoneOffset() * 60000 ))
         .toISOString()
         .split("T")[0]}`;
@@ -50,28 +47,28 @@ let state = {
       let day = dformat.getDay();
       let hour = dformat.getHours();
       let minute = dformat.getMinutes();
-      
+
       dformat.setMinutes(minute - (minute % 5))
-      
-      if(hour < 9 || (hour === 9 && minute < 35)) {
+
+      if(hour < 9 || hour === 9 && minute < 35) {
         dformat.setHours(16)
         dformat.setMinutes(0);
         dformat.setDate(dformat.getDate() - 1)
-      } else if (hour > 16 || (hour === 16 && minute > 0)) {
+      } else if (hour > 16 || hour === 16 && minute > 0) {
         dformat.setHours(16);
         dformat.setMinutes(0);
       }
-      
+
       if(day === 0)
         dformat.setDate(dformat.getDate() - 2);
-      else if (day === 6) 
+      else if (day === 6)
         dformat.setDate(dformat.getDate() - 1);
 
       return `${new Date(dformat.getTime() - (dformat.getTimezoneOffset() * 60000 ))
         .toISOString()
         .replace("T", " ")
         .substring(0, 19)}`;
-      
+
     }
   }
 }
@@ -80,7 +77,7 @@ let state = {
 
 let renderHome = () => {
   let main = document.querySelector("#main");
-  main.innerHTML = `<h1 id="parameter" class="text-center">Welcome to the MThree Stock Analysis Application</h1>`;
+  main.innerHTML = `<h1 id="parameter" class="text-center">Welcome to the EmThree Stock Analysis Application</h1>`;
 }
 
 let renderAllTable = mode => {
@@ -111,19 +108,19 @@ let renderAllTable = mode => {
   </table>`;
 };
 
-let renderSingleInputForm = async mode => {
+let renderInputForm = async mode => {
   let inputType = state.singleInputType[mode]
   let data = state.data[mode];
   document.querySelector("#main").innerHTML =
   `<h1 id="parameter" class="text-center">${mode} - Please select your parameters:</h1>
   <div class="d-flex flex-column align-items-center">
-  <form id="userinput">
-    <input  type=${inputType}
-            name="param"
-            max=${data[0].date.replace(" ","T")}
-            min=${data[data.length - 1].date.replace(" ","T")}>
-    <input type="submit">
-  </form>
+    <form id="userinput">
+      <input  type=${inputType}
+              name="param"
+              max=${data[0].date.replace(" ","T")}
+              min=${data[data.length - 1].date.replace(" ","T")}>
+      <input type="submit">
+    </form>
   </div>`;
 };
 
@@ -142,7 +139,7 @@ let renderSingleInputTable = (mode, data) => {
     <tr>
       <td>${d.toUpperCase()}</td>
       ${d !== 'volume' ?
-      `<td>${data[d]}</td>` : 
+      `<td>${data[d]}</td>` :
       `<td>${Number(data.volume).toLocaleString()}</td>`}
     </tr>`).join('')}
   </tbody>
@@ -154,23 +151,8 @@ let renderHistorical = () => {
   <h1 id="parameter" class="text-center">Historical Stock Price for ${state.selection.Company}</h1>
   <div class="historical-line-container border border-primary rounded"></div>
   <div class="historical-volume-container border border-primary rounded"></div>`
-}
 
-// Dynamic Methods
-
-let armSubmit = mode => {
-  let userinput = document.querySelector("#userinput");
-  userinput.addEventListener('submit', e => {
-    e.preventDefault();
-    let input = mode !== 'Intraday' ? e.target.param.value : `${e.target.param.value.replace("T"," ")}:00`;
-    input = state.inputValidation[mode](input);
-    let data = state.data[mode].find(w => w.date === input);
-    renderSingleInputTable(mode, data);
-  });
-};
-
-let armHistorical = () => {
-  const lineData = {
+  return {
     dataByTopic: [
         {
           topic: `Historical Data of ${state.selection.Company}`,
@@ -182,33 +164,52 @@ let armHistorical = () => {
       }
     ]
   };
+}
 
-  const volumeData = state.data.Weekly.map(w => ({
-    value: w.close,
-    date: new Date(`${w.date} GMT-05:00`)
-  }))
+let renderDayTradePlatform = date => {
+  const data = state.data.Intraday;
+  document.querySelector("#main").innerHTML = `
+  <h1 id="parameter" class="text-center">Day Trade Platform for ${state.selection.Company}</h1>
+  <div id='day-trade' style='height: 500px'></div>
+  <div class="d-flex flex-column align-items-center">
+     <form id="userinput">Selected Date:
+       <input  type="date"
+               name="param"
+               value=${data[0].date.split(" ")[0]}
+               max=${data[0].date.split(" ")[0]}
+               min=${data[data.length - 1].date.split(" ")[0]}>
+       <input  type="submit"
+               value="Re-Render"/>
+     </form>
+   </div>`
+  return date ? date : state.data.Intraday[0].date.split(" ")[0];
+}
+
+// HISTORICAL
+
+const armHistorical = lineData => {
+
+  const volumeData = lineData.dataByTopic[0].dates
 
   const linecontainer = d3.select('.historical-line-container');
-  const volumecontainer = d3.select('.historical-volume-container');
+  const selectorContainer = d3.select('.historical-volume-container');
   const containerWidth = linecontainer.node().getBoundingClientRect().width;
   const lineHistorical = britecharts.line();
   const brushHistorical = britecharts.brush();
-  const chartTooltip = britecharts.tooltip();
 
   lineHistorical
     .isAnimated(true)
     .grid('full')
     .width(containerWidth)
     .height(350)
-    .xAxisLabel("Date")
     .xAxisFormat(britecharts.line().axisTimeCombinations.CUSTOM)
-    .xAxisCustomFormat("%Y");
-    
+    .xAxisCustomFormat("%m %Y")
+
   brushHistorical
     .width(containerWidth)
     .height(100)
     .xAxisFormat(britecharts.brush().axisTimeCombinations.CUSTOM)
-    .xAxisCustomFormat("%Y")
+    .xAxisCustomFormat("%m %Y")
     .margin({top:0, bottom: 40, left: 50, right: 30})
     .on('customBrushEnd', ([brushStart, brushEnd]) => {
       if (brushStart && brushEnd) {
@@ -216,29 +217,95 @@ let armHistorical = () => {
         view.dataByTopic[0].dates = lineData.dataByTopic[0].dates.filter(w => (
           new Date(`${w.date} GMT-05:00`) > new Date(`${brushStart}`) &&
           new Date(`${w.date} GMT-05:00`) < new Date(`${brushEnd}`)
-          ))
-          linecontainer.datum(view).call(lineHistorical);
+        ))
+        linecontainer.datum(view).call(lineHistorical);
       }
   });
 
   linecontainer.datum(lineData).call(lineHistorical);
-  volumecontainer.datum(volumeData).call(brushHistorical);
-
-  const tooltipContainer = d3.select('.historical-line-container .metadata-group ');
-  tooltipContainer.call(chartTooltip);
-
-  const redrawHistorical = () => {
-    const newContainerWidth = linecontainer.node() ? linecontainer.node().getBoundingClientRect().width : false;
-    lineHistorical.width(newContainerWidth);
-    linecontainer.call(lineHistorical);
-  };
-  const throttledRedraw = _.throttle(redrawHistorical, 200);
-
-  window.addEventListener("resize", throttledRedraw);
+  selectorContainer.datum(volumeData).call(brushHistorical);
 
 };
 
-// Fetches
+// DAY TRADE
+
+const candlestickSeries = fc.seriesSvgCandlestick()
+	.bandwidth(3);
+
+const movingAverageSeries = fc.seriesSvgLine()
+  .mainValue(d => d.ma)
+  .crossValue(d => d.date);
+
+const mergedSeries = fc.seriesSvgMulti()
+	.series([movingAverageSeries, candlestickSeries]);
+
+const xScale = fc.scaleDiscontinuous(d3.scaleTime())
+  .discontinuityProvider(fc.discontinuitySkipWeekends());
+
+const chart = fc.chartCartesian(
+    xScale,
+    d3.scaleLinear()
+  )
+	.yOrient('left')
+	.svgPlotArea(mergedSeries);
+
+const durationDay = 864e5;
+const xExtent = fc.extentDate()
+	.accessors([d => d.date])
+
+const yExtent = fc.extentLinear()
+	.accessors([d => d.high, d => d.low])
+	.pad([0.1, 0.1]);
+
+const parseDate = d3.timeParse("%m-%Y");
+
+const ma = fc.indicatorMovingAverage()
+    .value(d => d.open);
+
+const armDayTradePlatform = day => {
+  d3.csv(`http://localhost:4567/getIntraday/tradealgo/${day}`,
+    row => ({
+      open: Number(row.Open),
+      close: Number(row.Close),
+      high: Number(row.High),
+      low: Number(row.Low),
+      date: new Date(row.Date)
+    })).then(data => {
+      const maData = ma(data);
+      const mergedData = data.map((d, i) =>
+        Object.assign({}, d, {
+          ma: maData[i]
+        })
+      );
+     
+      chart.xDomain(xExtent(mergedData))
+        .yDomain(yExtent(mergedData))
+  
+      d3.select('#day-trade')
+        .datum(mergedData)
+        .call(chart);
+    });
+}
+
+// DYNAMIC METHODS
+
+let armSubmit = mode => {
+  let userinput = document.querySelector("#userinput");
+  userinput.addEventListener('submit', e => {
+    e.preventDefault();
+    if(mode === 'daytrade') {
+      armDayTradePlatform(renderDayTradePlatform(state.inputValidation.Daily((e.target.param.value))));
+      armSubmit('daytrade');
+    } else {
+      let input = mode !== 'Intraday' ? e.target.param.value : `${e.target.param.value.replace("T"," ")}:00`;
+      input = state.inputValidation[mode](input);
+      let data = state.data[mode].find(w => w.date === input);
+      renderSingleInputTable(mode, data);
+    }
+  });
+};
+
+// FETCHES
 
 let getAllDataCSV = () => {
   fetch("http://localhost:4567/getWeekly")
@@ -254,25 +321,7 @@ let getAllDataCSV = () => {
   .then(csv => loadFromCSV(csv, 'Intraday'));
 }
 
-// let getSingleWeekly = week => {
-//   fetch(`http://localhost:4567/getWeekly/${week}`)
-//   .then(response => response.text())
-//   .then(csv => displayTable(csv));
-// };
-
-// let getSingleDaily = day => {
-//   fetch(`http://localhost:4567/getDaily/${day}`)
-//   .then(response => response.text())
-//   .then(csv => displayTable(csv));
-// };
-
-// let getSingleIntraday = minute => {
-//   fetch(`http://localhost:4567/getIntraday/${minute}`)
-//     .then(response => response.text())
-//     .then(csv => displayTable(csv));
-// };
-
-// State Modification
+// STATE MODIFICATION
 
 let loadFromCSV = (data, table) => {
   data.split('\n')
@@ -349,7 +398,7 @@ let dttests = () => {
   console.log(state.inputValidation.Intraday("2020-02-06 03:00:00"));
 };
 
-// OnLoad
+// ONLOAD
 
 let onLoad = async () => {
   await getAllDataCSV();
